54206279d3bd38d4e3c547cf76cf93b9a357d903
Matthew Holden <matthew.holden@mindbodyonline.com>
2013-11-14 14:23:57 -0800

    More crap

---
+++ Charity/Charity/CCFBRequestManager.h
@@ -0,0 +1,13 @@
+//
+//  CCFBRequestManager.h
+//  Charity
+//
+//  Created by Matthew Holden on 11/14/13.
+//  Copyright (c) 2013 CharityChallenge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCFBRequestManager : NSObject
+
+@end
---
+++ Charity/Charity/CCFBRequestManager.m
@@ -0,0 +1,13 @@
+//
+//  CCFBRequestManager.m
+//  Charity
+//
+//  Created by Matthew Holden on 11/14/13.
+//  Copyright (c) 2013 CharityChallenge. All rights reserved.
+//
+
+#import "CCFBRequestManager.h"
+
+@implementation CCFBRequestManager
+
+@end
