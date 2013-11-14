3749d1c773a43b7bdc7434eeb77f29c6536a0d09
John Hammerlund <johnhammerlund@gmail.com>
2013-11-14 13:36:14 -0800

    Add basic class name constants and import in prefix header

---
+++ Charity/Charity/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  Charity
+//
+//  Created by John Hammerlund on 11/14/13.
+//  Copyright (c) 2013 CharityChallenge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const kUserPFObject;
+extern NSString * const kChallengePFObject;
+extern NSString * const kDonationPFObject;
+extern NSString * const kCharityPFObject;
+extern NSString * const kInvitationPFObject;
---
+++ Charity/Charity/Constants.m
@@ -0,0 +1,15 @@
+//
+//  Constants.m
+//  Charity
+//
+//  Created by John Hammerlund on 11/14/13.
+//  Copyright (c) 2013 CharityChallenge. All rights reserved.
+//
+
+#import "Constants.h"
+
+NSString * const kUserPFObject = @"User";
+NSString * const kChallengePFObject = @"Challenge";
+NSString * const kDonationPFObject = @"Donation";
+NSString * const kCharityPFObject = @"Charity";
+NSString * const kInvitationPFObject = @"Invitation";
