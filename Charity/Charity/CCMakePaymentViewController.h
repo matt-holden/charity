3d5d88120fae5273d5a2004295d8736596d3f18d
Matthew Holden <matthew.holden@mindbodyonline.com>
2013-11-14 17:33:34 -0800

    MORE

---
+++ Charity/Charity/CCMakePaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCMakePaymentViewController.h
+//  Charity
+//
+//  Created by Matthew Holden on 11/14/13.
+//  Copyright (c) 2013 CharityChallenge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCMakePaymentViewController : UIViewController
+
+@end
---
+++ Charity/Charity/CCMakePaymentViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCMakePaymentViewController.m
+//  Charity
+//
+//  Created by Matthew Holden on 11/14/13.
+//  Copyright (c) 2013 CharityChallenge. All rights reserved.
+//
+
+#import "CCMakePaymentViewController.h"
+
+@interface CCMakePaymentViewController ()
+
+@end
+
+@implementation CCMakePaymentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
