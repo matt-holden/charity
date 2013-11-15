bf04fe5237393751a81af82af4e564f1d1657c38
John Hammerlund <johnhammerlund@gmail.com>
2013-11-14 18:16:47 -0800

    Add a bunch of stuff to challenges VC

--- Charity/Charity/CCChallengesTableViewController.m
+++ Charity/Charity/CCChallengesTableViewController.m
@@ -14,25 +14,76 @@
 
 @implementation CCChallengesTableViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
+    [self setParseClassName:kChallengePFObject];
+    [self setPullToRefreshEnabled:YES];
+    [self setPaginationEnabled:NO];
+    [self setObjectsPerPage:25];
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (PFQuery *)queryForTable {
+    PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+    [query includeKey:@"donors"];
+    [query includeKey:@"endDate"];
+    [query includeKey:@"charity"];
+    [query whereKey:@"donors" equalTo:[PFUser currentUser]];
+
+
+    // If no objects are loaded in memory, we look to the cache
+    // first to fill the table and then subsequently do a query
+    // against the network.
+    if ([self.objects count] == 0) {
+        [query setCachePolicy:kPFCachePolicyCacheThenNetwork];
+    }
+
+    //TODO - Contant-ize table columns
+    [query orderByDescending:@"endDate"];
+
+    return query;
 }
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+                        object:(PFObject *)object {
+    static NSString *CellIdentifier = @"challengeCell";
+
+    PFTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:CellIdentifier];
+        [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
+        [cell.detailTextLabel setLineBreakMode:NSLineBreakByWordWrapping];
+    }
+
+    unsigned long numberOfDonors = [(NSArray *)object[@"donors"] count];
+    [cell.textLabel setText:[NSString stringWithFormat:@"%lu %@ donated!", numberOfDonors, (numberOfDonors > 1 ? @"people have" : @"person has")]];
+
+    NSDate *endDate = object[@"endDate"];
+
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [calendar components:(NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate:[NSDate date] toDate:endDate options:0];
+
+    NSInteger daysLeft = components.day;
+    NSInteger hoursLeft = components.hour;
+    NSInteger minutesLeft = components.minute;
+
+    NSString *daysLeftString = daysLeft > 0 ? daysLeft == 1 ? @"1 day, " : [NSString stringWithFormat:@"%ld days, ", daysLeft] : @"";
+NSString *hoursLeftString = hoursLeft > 0 ? hoursLeft == 1 ? @"1 hour, " : [NSString stringWithFormat:@"%ld hours, ", daysLeft] : @"";
+NSString *minutesLeftString = minutesLeft > 0 ? minutesLeft == 1 ? @"1 minute" : [NSString stringWithFormat:@"%ld minutes", daysLeft] : @"";
+
+
+    [cell.detailTextLabel setText:[NSString stringWithFormat:@"%@%@%@ remaining!", daysLeftString, hoursLeftString, minutesLeftString]];
+
+    return cell;
+}
+
+
 
 @end
