73131e52084948b3fc7864d1578e92fdf6306480
Matthew Holden <matthew.holden@mindbodyonline.com>
2013-11-14 16:47:31 -0800

    More

--- Charity/Charity/CCCreateChallenge2ViewController.m
+++ Charity/Charity/CCCreateChallenge2ViewController.m
@@ -7,12 +7,37 @@
 //
 
 #import "CCCreateChallenge2ViewController.h"
+#import "CCFBRequestManager.h"
 
-@interface CCCreateChallenge2ViewController ()
+@interface CCCreateChallenge2ViewController () <UIAlertViewDelegate, UITextFieldDelegate, UITextViewDelegate>
 
 @end
 
 @implementation CCCreateChallenge2ViewController
 
+- (IBAction)chooseTenTapped:(id)sender {
+    [[CCFBRequestManager new] sendRequest];
+}
+- (IBAction)nextTapped:(id)sender {
+    [[[UIAlertView alloc] initWithTitle:@"Done" message:@"Created" delegate:self cancelButtonTitle:@"Cool" otherButtonTitles:nil] show];
+}
 
+
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == alertView.cancelButtonIndex) {
+        [self.navigationController popToRootViewControllerAnimated:YES];
+    }
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self resignFirstResponder];
+    return YES;
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    [self resignFirstResponder];
+}
 @end
